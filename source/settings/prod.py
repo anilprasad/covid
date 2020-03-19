62bc0d8d7341aa800d4653dd153c9cda48536100
Calin Rada <rada.calin@gmail.com>
2020-03-19 03:31:14 +0000

    Production will always use PostgreSQL

--- source/settings/prod.py
+++ source/settings/prod.py
@@ -9,3 +9,14 @@ COMPRESS_ENABLED = False
 # django-htmlmin settings
 HTML_MINIFY = True
 KEEP_COMMENTS_ON_MINIFYING = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('APP_DB_NAME', 'app_dev'),
+        'USER': os.environ.get('APP_DB_USER', 'root'),
+        'PASSWORD': os.environ.get('APP_DB_PASSWORD', None),
+        'HOST': os.environ.get('APP_DB_HOST', '192.168.33.11'),
+        'PORT': os.environ.get('APP_DB_PORT', '3306'),
+    }
+}
