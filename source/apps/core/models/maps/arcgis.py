cd65c5798e80fbc22085a86be4598b674d375470
Calin Rada <rada.calin@gmail.com>
2020-03-20 17:07:07 +0000

    Added image field for Arcgis maps. Added og_ meta block in arcgis layout.

--- source/apps/core/models/maps/arcgis.py
+++ source/apps/core/models/maps/arcgis.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 from source.apps.core.models.timestamped import TimestampedModel
 from source.apps.core.models.slugable import SlugableModel
+from source.apps.core.media import scramble_uploaded_filename
 
 
 class MapsArcgisModel(TimestampedModel, SlugableModel):
@@ -9,9 +11,13 @@ class MapsArcgisModel(TimestampedModel, SlugableModel):
     name = models.CharField(max_length=64)
     slug = models.SlugField(max_length=72)
     url = models.URLField()
+    image = models.ImageField(null=True, blank=True, upload_to=scramble_uploaded_filename)
 
     def __str__(self):
         return '%s map' % self.name
+
+    def get_absolute_url(self):
+        return reverse('map_arcgis', kwargs={'country': self.slug})
 
     class Meta:
         db_table = 'maps_arcgis'
